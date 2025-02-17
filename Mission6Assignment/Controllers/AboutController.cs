a368e7053fe7852bc76a9bcaacc63c97b659ae41
mbement6 <mclainbement@gmail.com>
2025-02-17 12:10:27 -0700

    fixing the structure

---
+++ Mission6Assignment/Controllers/AboutController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mission6Assignment.Controllers
+{
+    public class AboutController : Controller
+    {
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
